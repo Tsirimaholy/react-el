cc06fa79d2ed0419d6ead3727a8926e7f8f274d4
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-09-29 13:34:35 +0300

    feat: basic form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useForm} from "react-hook-form"
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
+
+
+
+const schema = yup
+    .object({
+        author: yup.string().required(),
+        email: yup.string().email(),
+        content: yup.string()
+    })
+    .required()
 
 function App() {
-  const [count, setCount] = useState(0)
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm({
+        resolver: yupResolver(schema),
+    })
+    const onSubmit = (data) => console.log(data)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("author")} placeholder={"author"}/>
+            <p>{errors.author?.message}</p>
+
+            <input {...register("email")} type="email"/>
+            <p>{errors.email?.message}</p>
+            <textarea {...register("content")}/>
+            <p>{errors.content?.message}</p>
+            <input type="submit" value={"Publier"}/>
+        </form>
+    )
 }
 
 export default App
